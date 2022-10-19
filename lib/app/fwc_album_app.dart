3c705dd3512f7f4829acc9e9c05e9dfc909168f3
bruno <bruno@colaco.com.br>
2022-10-18 23:18:25 -0300

    Finaliza aula 3 - Com problema

--- lib/app/fwc_album_app.dart
+++ lib/app/fwc_album_app.dart
@@ -6,7 +6,8 @@ import 'package:fwc_album_app/app/core/ui/global/global_context_impl.dart';
 import 'package:fwc_album_app/app/core/ui/theme/theme_config.dart';
 import 'package:fwc_album_app/app/pages/auth/login/login_route.dart';
 import 'package:fwc_album_app/app/pages/auth/register/register_route.dart';
-import 'package:fwc_album_app/app/pages/home/home_page.dart';
+import 'package:fwc_album_app/app/pages/home/home_route.dart';
+import 'package:fwc_album_app/app/pages/my_stickers/my_sticker_page.dart';
 import 'package:fwc_album_app/app/pages/splash/splash_route.dart';
 import 'package:fwc_album_app/app/repository/auth/auth_repository.dart';
 import 'package:fwc_album_app/app/repository/auth/auth_repository_impl.dart';
@@ -22,13 +23,15 @@ class FwcAlbumApp extends StatelessWidget {
       bindingsBuilder: () => [
         Bind.lazySingleton<CustomDio>((i) => CustomDio()),
         Bind.lazySingleton<AuthRepository>((i) => AuthRepositoryImpl(dio: i())),
-        Bind.lazySingleton<GlobalContext>((i) => GlobalContextImpl(
-              navigatorKey: navigatorKey,
-              authRepository: i(),
-            ))
+        Bind.lazySingleton<GlobalContext>(
+          (i) => GlobalContextImpl(
+            navigatorKey: navigatorKey,
+            authRepository: i(),
+          ),
+        )
       ],
       child: MaterialApp(
-        title: 'Fifa Wordl Cup Album',
+        title: 'Fifa World Cup Album',
         debugShowCheckedModeBanner: false,
         navigatorKey: navigatorKey,
         theme: ThemeConfig.theme,
@@ -36,7 +39,8 @@ class FwcAlbumApp extends StatelessWidget {
           '/': (_) => const SplashRoute(),
           '/auth/login': (_) => const LoginRoute(),
           '/auth/register': (_) => const RegisterRoute(),
-          '/home': (_) => const HomePage(),
+          '/home': (_) => const HomeRoute(),
+          '/my-stickers': (_) => const MyStickerPage(),
         },
       ),
     );
--- lib/app/pages/home/home_page.dart
+++ lib/app/pages/home/home_page.dart
@@ -1,17 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_getit/flutter_getit.dart';
-import 'package:fwc_album_app/app/core/rest/custom_dio.dart';
 import 'package:fwc_album_app/app/core/ui/styles/button_styles.dart';
 import 'package:fwc_album_app/app/core/ui/styles/colors_app.dart';
 import 'package:fwc_album_app/app/core/ui/styles/text_styles.dart';
 import 'package:fwc_album_app/app/core/ui/widgets/button.dart';
+import 'package:fwc_album_app/app/pages/home/presenter/home_presenter.dart';
+import 'package:fwc_album_app/app/pages/home/view/home_view_impl.dart';
 import 'package:fwc_album_app/app/pages/home/widgets/status_tile.dart';
 import 'package:fwc_album_app/app/pages/home/widgets/sticker_percent_widget.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class HomePage extends StatefulWidget {
+  final HomePresenter presenter;
 
+  const HomePage({super.key, required this.presenter});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends HomeViewImpl {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,97 +27,103 @@ class HomePage extends StatelessWidget {
         backgroundColor: context.colors.primary,
         actions: [
           IconButton(
-            onPressed: () {},
-            icon: const Icon(
-              Icons.logout,
-              color: Colors.white,
-            ),
-          )
+              onPressed: () => widget.presenter.logout(),
+              icon: const Icon(
+                Icons.logout,
+                color: Colors.white,
+              ))
         ],
       ),
       body: Container(
-          decoration: const BoxDecoration(
-            image: DecorationImage(
-                image: AssetImage('assests/images/background.png'),
-                fit: BoxFit.cover),
-          ),
-          child: LayoutBuilder(
-            builder: (_, constrains) {
-              return ConstrainedBox(
-                constraints: BoxConstraints(minHeight: constrains.maxHeight),
-                child: Center(
-                  child: SingleChildScrollView(
-                    child: Column(
-                      children: [
-                        Container(
-                          padding: const EdgeInsets.only(bottom: 35),
-                          width: MediaQuery.of(context).size.width,
-                          child: Image.asset(
-                            'assets/images/bola.png',
-                            fit: BoxFit.cover,
-                          ),
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('assets/images/background.png'),
+              fit: BoxFit.cover),
+        ),
+        child: LayoutBuilder(
+          builder: (_, constrains) {
+            return ConstrainedBox(
+              constraints: BoxConstraints(minHeight: constrains.maxHeight),
+              child: Center(
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.only(bottom: 35),
+                        width: MediaQuery.of(context).size.width,
+                        child: Image.asset(
+                          'assets/images/bola.png',
+                          fit: BoxFit.cover,
                         ),
+                      ),
 // ****************** PERCENT
-                        StickerPercentWidget(
-                          percent: 60,
-                        ),
+                      StickerPercentWidget(
+                        percent: user?.totalCompletePercent ?? 0,
+                      ),
 // ****************** SEPARADOR
-                        const SizedBox(
-                          height: 20,
-                        ),
+                      const SizedBox(
+                        height: 20,
+                      ),
 // ****************** TÍTULO
-                        Text(
-                          '45 Figurinhas',
-                          style: context.textStyles.titleWhite,
-                        ),
+                      Text(
+                        '${user?.totalStickers ?? 0} figurinhas',
+                        style: context.textStyles.titleWhite,
+                      ),
 // ****************** SEPARADOR
-                        const SizedBox(
-                          height: 20,
-                        ),
+                      const SizedBox(
+                        height: 20,
+                      ),
 // ****************** TODAS
-                        StatusTile(
-                            icon: Image.asset('assets/images/all_icon.png'),
-                            label: 'Todas',
-                            value: 34),
+                      StatusTile(
+                        icon: Image.asset('assets/images/all_icon.png'),
+                        label: 'Todas',
+                        value: user?.totalAlbum ?? 0,
+                      ),
 // ****************** SEPARADOR
-                        const SizedBox(
-                          height: 20,
-                        ),
+                      const SizedBox(
+                        height: 20,
+                      ),
 // ****************** FALTANDO
-                        StatusTile(
-                            icon: Image.asset('assets/images/missing_icon.png'),
-                            label: 'Faltando',
-                            value: 500),
+                      StatusTile(
+                        icon: Image.asset('assets/images/missing_icon.png'),
+                        label: 'Faltando',
+                        value: user?.totalComplete ?? 0,
+                      ),
 // ****************** SEPARADOR
-                        const SizedBox(
-                          height: 20,
-                        ),
+                      const SizedBox(
+                        height: 20,
+                      ),
 // ****************** REPETIDAS
-                        StatusTile(
-                            icon: Image.asset('assets/images/repeted_icon.png'),
-                            label: 'Repetidas',
-                            value: 30),
+                      StatusTile(
+                        icon: Image.asset('assets/images/repeated_icon.png'),
+                        label: 'Repetidas',
+                        value: user?.totalDuplicates ?? 0,
+                      ),
 // ****************** SEPARADOR
-                        const SizedBox(
-                          height: 20,
-                        ),
+                      const SizedBox(
+                        height: 20,
+                      ),
 // ****************** SEPARADOR
-                        Button(
-                          onPressed: () {},
-                          width: MediaQuery.of(context).size.width * .9,
-                          style: context.buttonStyles.yellowOutlineButton,
-                          labelStyle: context
-                              .textStyles.textSecondaryFontExtraBold
-                              .copyWith(color: context.colors.yellow),
-                          label: 'Minhas Figurinhas',
-                        )
-                      ],
-                    ),
+                      Button(
+                        onPressed: () {
+                          Navigator.of(context).pushNamed('/my-stickers');
+                        },
+                        width: MediaQuery.of(context).size.width * .9,
+                        outline: true,
+                        style: context.buttonStyles.yellowOutlineButton,
+                        labelStyle: context
+                            .textStyles.textSecondaryFontExtraBold
+                            .copyWith(color: context.colors.yellow),
+                        label: 'Minhas Figurinhas',
+                      ),
+                    ],
                   ),
                 ),
-              );
-            },
-          )),
+              ),
+            );
+          },
+        ),
+      ),
     );
   }
 }
